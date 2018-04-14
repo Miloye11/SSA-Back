be6f375bd14e5320b5c120f02a95a9f859a3ffe3
DESKTOP-SCGU0IL\Marko <marko.vukovickg95@gmail.com>
2018-04-14 13:32:44 +0200

    Odradjena klasa Person u Models u Data Layer-u, kao i odradjen PersonRepository sa metodama za Insert, Update, Select nad tabelom Persons.

---
+++ WebApi/DataLayer/IPersonRepository.cs
@@ -0,0 +1,16 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+   public interface IPersonRepository
+    {
+        List<Person> GetAllPersons();
+        int InsertPersons(Person p);
+        int UpdatePersons(Person p);
+    }
+}
---
+++ WebApi/DataLayer/PersonRepository.cs
@@ -0,0 +1,78 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+   public class PersonRepository : IPersonRepository
+    {
+        //konekcioni string
+        private string ConnectionString = ConfigurationManager.AppSettings["ConnectionString"];
+
+        //metoda za vracanje svih osoba iz tabele Persons
+        public List<Person> GetAllPersons()
+        {
+            List<Person> listToReturn = new List<Person>();
+            using (SqlConnection dataConnection = new SqlConnection(this.ConnectionString))
+            {
+                dataConnection.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = dataConnection;
+                command.CommandText = "SELECT * FROM Persons";
+
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    Person p = new Person();
+                    p.Person_Id = dataReader.GetInt32(0);
+                    p.Name = dataReader.GetString(1);
+                    p.Surname = dataReader.GetString(2);
+                    p.JMBG = dataReader.GetString(3);
+                    p.Card_Number = dataReader.GetInt32(4);
+                    listToReturn.Add(p);
+                }
+            }
+            return listToReturn;
+        }
+
+
+        //metoda za ubacivanje nove osobe u tabelu Persons
+        public int InsertPersons(Person p)
+        {
+            using (SqlConnection dataConnection = new SqlConnection(ConnectionString))
+            {
+                dataConnection.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = dataConnection;
+                command.CommandText = "INSERT INTO Persons VALUES ('" + p.Name + "', '" + p.Surname + "', '" + p.JMBG + "'," + p.Card_Number + ")";
+
+                return command.ExecuteNonQuery();
+            }
+        }
+
+
+        //metoda za update/azuriranje osoba u tabeli Persons
+        public int UpdatePersons(Person p)
+        {
+            using (SqlConnection dataConnection = new SqlConnection(ConnectionString))
+            {
+                dataConnection.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = dataConnection;
+                command.CommandText = "UPDATE Persons SET Name ='" + p.Name + "', Surname='" + p.Surname + "', JMBG = '" + p.JMBG + "', Card_Number = " + p.Card_Number + " WHERE Person_Id = " + p.Person_Id + " ";
+
+                return command.ExecuteNonQuery();
+            }
+        }
+
+    }
+}
